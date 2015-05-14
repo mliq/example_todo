7b5100177a792e20e9d4e54ce7752015759a0f28
mliq <liquori@gmail.com>
2015-05-14 14:02:02 -0500

    Kind of a mess

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,19 @@
 var express = require('express');
 var app = express.Router();
-var mongoose = require('mongoose');
 var Todo = require('../models/Todo.js');
 var path = require('path');
 
-app.get("/", function(req,res,next){
-  res.sendFile(path.resolve(__dirname, '../views/index.html'));
+app.get("/", function (req, res, next) {
+    res.sendFile(path.resolve(__dirname, '../views/index.html'));
 });
 
-app.post("/", function(req,res,next){
-  Todo.create(req.body, function(err, post){
-    res.json(post);
-  })
+app.post("/", function (req, res, next) {
+    Todo.create(req.body, function (err, post) {
+        //res.json(post);
+        //res.sendFile(path.resolve(__dirname, '../views/index.html'));
+        res.redirect('/');
+    })
 });
 
-console.log('create loaded');
+console.log('index loaded');
 module.exports = app;
